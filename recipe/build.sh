81faaaf3233ab3de018ed17a9de4292abc2263e6
Sandro Dias Pinto Vitenti <sandro@isoftware.com.br>
2019-03-13 21:20:47 -0300

    Checking config.log

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ export PYTHON="python"
 
 ./configure --prefix="${PREFIX}" --enable-opt-cflags
 
+cat config.log
+
 make V=1 -j$CPU_COUNT
 #make check VERBOSE=1 -j$CPU_COUNT
 make install -j$CPU_COUNT
