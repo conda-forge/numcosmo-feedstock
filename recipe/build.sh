ac99b02a82f6ab8c5b6a66de932bd0edb3835a8f
Sandro Dias Pinto Vitenti <sandro@isoftware.com.br>
2019-03-19 09:49:28 -0300

    Debugging introspection detection.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,9 @@
 
 _PY=$PYTHON
 export PYTHON="python"
+export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
 
 ./configure --prefix="${PREFIX}" --enable-opt-cflags
-
-cat config.log
 
 make -j$CPU_COUNT
 #make check VERBOSE=1 -j$CPU_COUNT
