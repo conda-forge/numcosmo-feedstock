4f5fc0100770083a4fe3f5b1aad45ddf18ecc1d9
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2023-10-07 12:33:28 -0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,9 +11,16 @@ export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 export PKG_CONFIG=$(which pkg-config)
 
-./configure --prefix="${PREFIX}" --enable-opt-cflags || cat config.log || false
+./configure --prefix="${PREFIX}" \
+            --host=${HOST}       \
+            --build=${BUILD}     \
+            --enable-opt-cflags  \
+            || (cat config.log; false)
 
 make -j$CPU_COUNT
-#make check VERBOSE=1 -j$CPU_COUNT
+# if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+#   make check VERBOSE=1 -j$CPU_COUNT
+# fi
+
 make install -j$CPU_COUNT
 
