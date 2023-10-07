d9b050c3a99eb3d9d65adecf8a450a057d6d89dd
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2023-10-07 12:10:36 -0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
+set -ex
+
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 _PY=$PYTHON
 export PYTHON="python"
 export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG=$(which pkg-config)
 
 ./configure --prefix="${PREFIX}" --enable-opt-cflags || cat config.log || false
 
