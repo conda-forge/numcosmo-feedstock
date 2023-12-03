63c53ac75fcbf40351c44260c81af1b45a2542ee
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2023-12-03 11:54:13 -0300

    Test if gcc-ar and gcc-nm exists.

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,10 +9,14 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/
 export PKG_CONFIG=$(which pkg-config)
 
 # Workaround to use the right lto plugins
+
 # This should transform the last occurrence of 'ar' into 'gcc-ar'
-[[ $AR != *gcc* && $AR == *ar* ]] && AR="${AR%ar}gcc-ar${AR##*ar}"
+[[ $AR != *gcc* && $AR == *ar* ]] && export GCC_AR="${AR%ar}gcc-ar${AR##*ar}"
+[[ -e $GCC_AR ]] && AR=$GCC_AR
+
 # This should transform the last occurrence of 'nm' into 'gcc-nm'
-[[ $NM != *gcc* && $NM == *nm* ]] && NM="${NM%nm}gcc-nm${NM##*nm}"
+[[ $NM != *gcc* && $NM == *nm* ]] && export GCC_NM="${NM%nm}gcc-nm${NM##*nm}"
+[[ -e $GCC_NM ]] && AR=$GCC_NM
 
 if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
   unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
@@ -28,11 +32,15 @@ if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
     export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
     export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
 
-    # Workaround to use the right lto plugins, as above
+    # Workaround to use the right lto plugins
+
     # This should transform the last occurrence of 'ar' into 'gcc-ar'
-    [[ $AR != *gcc* && $AR == *ar* ]] && AR="${AR%ar}gcc-ar${AR##*ar}"
+    [[ $AR != *gcc* && $AR == *ar* ]] && export GCC_AR="${AR%ar}gcc-ar${AR##*ar}"
+    [[ -e $GCC_AR ]] && AR=$GCC_AR
+
     # This should transform the last occurrence of 'nm' into 'gcc-nm'
-    [[ $NM != *gcc* && $NM == *nm* ]] && NM="${NM%nm}gcc-nm${NM##*nm}"
+    [[ $NM != *gcc* && $NM == *nm* ]] && export GCC_NM="${NM%nm}gcc-nm${NM##*nm}"
+    [[ -e $GCC_NM ]] && AR=$GCC_NM
 
     # Unset them as we're ok with builds that are either slow or non-portable
     unset CFLAGS
